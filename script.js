475f5ab968a6cedc8273c269c58c5855d617a35b
M.Bilal Waris <bilalwaris543@gmail.com>
2026-01-01 23:32:58 +0500

    Added Text Color Changer

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+function btn1(){
+    document.getElementById("one").style.color = "blue"
+}
+
+function btn2(){
+    document.getElementById("two").style.color = "red"
+}
+
+function btn3(){
+    document.getElementById("three").style.color = "green"
+}
+
+function btn4(){
+    document.getElementById("four").style.color = "cornflowerblue"
+}
+
+function btn5(){
+    document.getElementById("five").style.color = "purple"
+}
+
+function btn6(){
+    document.getElementById("six").style.color = "brown"
+}
+
+// function textreset(){
+//     document.getElementById("one").style.color = "black"
+//     document.getElementById("two").style.color = "black"
+//     document.getElementById("three").style.color = "black"
+//     document.getElementById("four").style.color = "black"
+//     document.getElementById("five").style.color = "black"
+//     document.getElementById("six").style.color = "black"
+// }
+
+// function changecolor() {
+//     let r = Math.random() * 255
+//     let g = Math.random() * 255
+//     let b = Math.random() * 255
+//     document.body.style.backgroundColor = `rgb(${r},${g},${b})`
+// }
+
+// function bgreset(){
+//     document.body.style.backgroundColor = "white" 
+// }
+
+function reset(){
+   window.location.reload()
+}
