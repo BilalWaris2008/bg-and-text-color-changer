b8d4d3f7db43e4c2089e812bf972ede504136427
M.Bilal Waris <bilalwaris543@gmail.com>
2026-01-01 23:34:46 +0500

    Added Background Color Changer

--- script.js
+++ script.js
@@ -24,21 +24,21 @@ function btn6(){
     document.getElementById("six").style.color = "brown"
 }
 
-// function textreset(){
-//     document.getElementById("one").style.color = "black"
-//     document.getElementById("two").style.color = "black"
-//     document.getElementById("three").style.color = "black"
-//     document.getElementById("four").style.color = "black"
-//     document.getElementById("five").style.color = "black"
-//     document.getElementById("six").style.color = "black"
-// }
+function textreset(){
+    document.getElementById("one").style.color = "black"
+    document.getElementById("two").style.color = "black"
+    document.getElementById("three").style.color = "black"
+    document.getElementById("four").style.color = "black"
+    document.getElementById("five").style.color = "black"
+    document.getElementById("six").style.color = "black"
+}
 
-// function changecolor() {
-//     let r = Math.random() * 255
-//     let g = Math.random() * 255
-//     let b = Math.random() * 255
-//     document.body.style.backgroundColor = `rgb(${r},${g},${b})`
-// }
+function changecolor() {
+    let r = Math.random() * 255
+    let g = Math.random() * 255
+    let b = Math.random() * 255
+    document.body.style.backgroundColor = `rgb(${r},${g},${b})`
+}
 
 // function bgreset(){
 //     document.body.style.backgroundColor = "white" 
