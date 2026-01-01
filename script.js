5bcf5a5bcc8ccdcb6df3eeffe4cc5713925e29a3
M.Bilal Waris <bilalwaris543@gmail.com>
2026-01-01 23:37:24 +0500

    Added Reset All Functionality

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 
 
-function btn1(){
+function btn1() {
     document.getElementById("one").style.color = "blue"
 }
 
-function btn2(){
+function btn2() {
     document.getElementById("two").style.color = "red"
 }
 
-function btn3(){
+function btn3() {
     document.getElementById("three").style.color = "green"
 }
 
-function btn4(){
+function btn4() {
     document.getElementById("four").style.color = "cornflowerblue"
 }
 
-function btn5(){
+function btn5() {
     document.getElementById("five").style.color = "purple"
 }
 
-function btn6(){
+function btn6() {
     document.getElementById("six").style.color = "brown"
 }
 
-function textreset(){
+function textreset() {
     document.getElementById("one").style.color = "black"
     document.getElementById("two").style.color = "black"
     document.getElementById("three").style.color = "black"
@@ -40,10 +40,10 @@ function changecolor() {
     document.body.style.backgroundColor = `rgb(${r},${g},${b})`
 }
 
-// function bgreset(){
-//     document.body.style.backgroundColor = "white" 
-// }
+function bgreset() {
+    document.body.style.backgroundColor = "white"
+}
 
-function reset(){
-   window.location.reload()
+function reset() {
+    window.location.reload()
 }
